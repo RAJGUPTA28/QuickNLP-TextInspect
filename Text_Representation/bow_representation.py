64dd772a03245034206b35ed152168f562694199
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-11-23 09:36:24 +0530

    Update bow_representation.py

--- Text_Representation/bow_representation.py
+++ Text_Representation/bow_representation.py
@@ -11,3 +11,23 @@ documents = ["Machine learning uses historical data to predict output values.",
              "It is seen as a part of artificial intelligence.",
              "Machine learning programs can perform tasks without being explicitly programmed to do so."]
 print(documents)
+
+def preprocess(document):
+   #changes document to lower case and removes stopwords'
+
+    # change sentence to lower case
+    document = document.lower()
+
+    # tokenize into words
+    words = word_tokenize(document)
+
+    # remove stop words
+    words = [word for word in words if word not in stopwords.words("english")]
+
+    # join back words to make sentence
+    document = " ".join(words)
+    
+    return document
+
+documents = [preprocess(document) for document in documents]
+print(documents)
