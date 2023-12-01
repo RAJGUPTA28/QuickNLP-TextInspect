ea2cf2f92cf47a5d919aadd50502cd82234edb18
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-12-01 10:15:50 +0530

    Update method.py

--- Loading_Data/method.py
+++ Loading_Data/method.py
@@ -1,1 +1,11 @@
+# Load CSV (using python)
+import csv
+import numpy
+filename = 'pima-indians-diabetes.data.csv'
+raw_data = open(filename, 'rt')
+reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE)
+x = list(reader)
+data = numpy.array(x).astype('float')
+print(data.shape)
+
 
