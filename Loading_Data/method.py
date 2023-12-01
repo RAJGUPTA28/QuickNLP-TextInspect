cf40534620e52bed6b44adebcfa444a791c3bc81
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-12-01 10:17:34 +0530

    Update method.py

--- Loading_Data/method.py
+++ Loading_Data/method.py
@@ -16,3 +16,12 @@ url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indiansi
 raw_data = urlopen(url)
 dataset = loadtxt(raw_data, delimiter=",")
 print(dataset.shape)
+
+
+
+# Load CSV using Pandas from URL
+2 Import pandas
+3 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+4 names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+5 data = pandas.read_csv(url, names=names)
+6 print(data.shape)
