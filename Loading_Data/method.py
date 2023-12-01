9e44c7e41b51f16a38ca53c736e7f2ca6a12599a
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-12-01 10:16:17 +0530

    Update method.py

--- Loading_Data/method.py
+++ Loading_Data/method.py
@@ -9,3 +9,10 @@ data = numpy.array(x).astype('float')
 print(data.shape)
 
 
+# using Numpy
+from numpy import loadtxt
+from urllib.request import urlopen
+url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indiansiabetes.data.csv'
+raw_data = urlopen(url)
+dataset = loadtxt(raw_data, delimiter=",")
+print(dataset.shape)
