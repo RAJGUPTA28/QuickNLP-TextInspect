b9f3e964e0cd61b19c2ed5e01f8c1704ca50d38c
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-12-08 11:36:47 +0530

    Update word2vec_representation.py

--- Text_Representation/word2vec_representation.py
+++ Text_Representation/word2vec_representation.py
@@ -1,7 +1,7 @@
 
 
-# Python program to generate word vectors using Word2Vec
  
 # importing all necessary modules
 from nltk.tokenize import sent_tokenize, word_tokenize
-import warnings
+import gensim
+from gensim.models import Word2Vec
