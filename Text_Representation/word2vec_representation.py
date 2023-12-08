c0847bb995c0269da85d4f6a628fa724794372d6
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-12-08 11:37:37 +0530

    Update word2vec_representation.py

--- Text_Representation/word2vec_representation.py
+++ Text_Representation/word2vec_representation.py
@@ -5,3 +5,19 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
 import gensim
 from gensim.models import Word2Vec
+
+
+sample = open("C:\\Users\\Admin\\Desktop\\alice.txt", "utf8")
+s = sample.read()
+ 
+f = s.replace("\n", " ")
+ 
+data = []
+ 
+for i in sent_tokenize(f):
+    temp = []
+    # tokenize the sentence into words
+    for j in word_tokenize(i):
+        temp.append(j.lower())
+ 
+    data.append(temp)
