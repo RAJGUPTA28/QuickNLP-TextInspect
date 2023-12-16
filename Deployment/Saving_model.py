8b19551999ea22ebee6f07d177d3f88ce9a31f51
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-12-16 10:51:51 +0530

    Update Saving_model.py

--- Deployment/Saving_model.py
+++ Deployment/Saving_model.py
@@ -1,2 +1,26 @@
+from sklearn.neighbors import KNeighborsClassifier as KNN 
+import numpy as np 
+
+# Load dataset 
+from sklearn.datasets import load_iris 
+iris = load_iris() 
+
+X = iris.data 
+y = iris.target 
+
+# Split dataset into train and test 
+X_train, X_test, y_train, y_test = \ 
+	train_test_split(X, y, test_size=0.3, 
+					random_state=2018) 
+
+# import KNeighborsClassifier model 
+knn = KNN(n_neighbors=3) 
+
+# train model 
+knn.fit(X_train, y_train) 
+
+
+
 
 import pickle 
+saved_model = pickle.dumps(knn) 
