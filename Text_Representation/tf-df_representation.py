152c1b89e9e9df7ba71e428818d9849451bb7a1a
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-12-17 10:13:36 +0530

    Update tf-df_representation.py

--- Text_Representation/tf-df_representation.py
+++ Text_Representation/tf-df_representation.py
@@ -1,16 +1,11 @@
-# import required module
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-# assign documents
-d0 = 'Geeks for geeks'
 d1 = 'Geeks'
 d2 = 'r2j'
 
-# merge documents into a single corpus
-string = [d0, d1, d2]
+string = [d0, d1]
 
-# create object
+
 tfidf = TfidfVectorizer()
-
-# get tf-df values
 result = tfidf.fit_transform(string)
