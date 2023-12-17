fd307f1727bc163db48247c9c0dfbf1cb19965ef
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-12-17 10:12:45 +0530

    Update tf-df_representation.py

--- Text_Representation/tf-df_representation.py
+++ Text_Representation/tf-df_representation.py
@@ -1,3 +1,8 @@
 # import required module
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+# create object
+tfidf = TfidfVectorizer()
+
+# get tf-df values
+result = tfidf.fit_transform(string)
