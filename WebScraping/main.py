593b54436f1239fab93d7bacf9c37ede779d2781
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-12-30 12:03:07 +0530

    Update main.py

--- WebScraping/main.py
+++ WebScraping/main.py
@@ -1,6 +1,10 @@
 
 import requests 
+from bs4 import BeautifulSoup 
 
 # Making a GET request 
 r = requests.get('https://www.geeksforgeeks.org/python-programming-language/') 
   
+# Parsing the HTML 
+soup = BeautifulSoup(r.content, 'html.parser') 
+print(soup.prettify()) 
