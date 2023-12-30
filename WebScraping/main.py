c8ad008293156be88dbd78b0b6c59609e704f364
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-12-30 12:03:36 +0530

    Update main.py

--- WebScraping/main.py
+++ WebScraping/main.py
@@ -8,3 +8,13 @@ r = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
 # Parsing the HTML 
 soup = BeautifulSoup(r.content, 'html.parser') 
 print(soup.prettify()) 
+
+
+# Getting the title tag 
+print(soup.title) 
+  
+# Getting the name of the tag 
+print(soup.title.name) 
+  
+# Getting the name of parent tag 
+print(soup.title.parent.name) 
