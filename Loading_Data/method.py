01495591d4035e80ec4051621a0c2d7f700d8a22
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-12-31 12:03:43 +0530

    Update method.py

--- Loading_Data/method.py
+++ Loading_Data/method.py
@@ -29,15 +29,11 @@ print(data.shape)
 #load Data from SQL
 import sqlite3
 import pandas as pd
-
 conn = sqlite3.connect('test_database') 
-          
 sql_query = pd.read_sql_query ('''
                                SELECT
                                *
                                FROM products
                                ''', conn)
-
-
 df = pd.DataFrame(sql_query, columns = ['product_id', 'product_name', 'price'])
 print (df)
