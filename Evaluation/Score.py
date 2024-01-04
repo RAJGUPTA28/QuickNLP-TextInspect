6550730dc4a55bed62071b00ff7039839b3cbc80
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-01-04 12:10:02 +0530

    Update Score.py

--- Evaluation/Score.py
+++ Evaluation/Score.py
@@ -1,1 +1,10 @@
 
+from sklearn.metrics import accuracy_score
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+# Calculate accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+
+
