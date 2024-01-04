71892e327e7a5b65725a9a54c47c8483030aafad
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-01-04 12:13:49 +0530

    Update Score.py

--- Evaluation/Score.py
+++ Evaluation/Score.py
@@ -14,3 +14,16 @@ scores = cross_val_score(model, X, y, cv=5)
 # Calculate the average performance across all folds
 mean_accuracy = scores.mean()
 print("Mean Accuracy:", mean_accuracy)
+
+
+from sklearn.metrics import precision_score,\ 
+recall_score, f1_score, accuracy_score
+
+
+print("Precision:", precision_score(y_test, 
+                                    y_pred, 
+                                    average="weighted")) 
+  
+print('Recall:', recall_score(y_test, 
+                              y_pred, 
+                              average="weighted")) 
