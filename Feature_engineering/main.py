607d49f5c8121e414f54995111244a00b5fa8b9d
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-01-06 12:23:47 +0530

    Update main.py

--- Feature_engineering/main.py
+++ Feature_engineering/main.py
@@ -1,10 +1,27 @@
 # 1. Number of Characters
-
 def count_chars(text):
     return len(text)
 
 # 2. Number of words
-
 def count_words(text):
     return len(text.split())
 
+# 3. Count Capital Letter
+def count_capital_chars(text):
+    count=0
+    for i in text:
+        if i.isupper():
+            count+=1
+    return count
+
+# 4.Capital Words
+def count_capital_words(text):
+    return sum(map(str.isupper,text.split()))
+
+# 5.Count Numberr of Puncuation
+def count_punctuations(text):
+    punctuations='!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'
+    d=dict()
+    for i in punctuations:
+        d[str(i)+' count']=text.count(i)
+    return d 
