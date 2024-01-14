456c0d58f35119fba367a52267accb1afccadcb2
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-01-14 12:13:27 +0530

    Update method.py

--- Loading_Data/method.py
+++ Loading_Data/method.py
@@ -19,12 +19,6 @@ print(dataset.shape)
 
 
 
-# Load CSV using Pandas from URL
-Import pandas
-url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
-names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
-data = pandas.read_csv(url, names=names)
-print(data.shape)
 
 #load Data from SQL
 import sqlite3
