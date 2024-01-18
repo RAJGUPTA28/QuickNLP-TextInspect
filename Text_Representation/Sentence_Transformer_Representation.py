25ce6737bfc9126e40b341dc52239a70b32c8dc3
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-01-18 11:48:42 +0530

    Update Sentence_Transformer_Representation.py

--- Text_Representation/Sentence_Transformer_Representation.py
+++ Text_Representation/Sentence_Transformer_Representation.py
@@ -2,3 +2,8 @@
 
 from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('all-MiniLM-L6-v2')
+
+sentences = ['This framework generates embeddings for each input sentence',
+    'Sentences are passed as a list of string.', 
+    'The quick brown fox jumps over the lazy dog.']
+sentence_embeddings = model.encode(sentences)
