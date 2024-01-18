89827267898a7de6eb677c844409af9b4de09ce4
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-01-18 11:48:57 +0530

    Update Sentence_Transformer_Representation.py

--- Text_Representation/Sentence_Transformer_Representation.py
+++ Text_Representation/Sentence_Transformer_Representation.py
@@ -7,3 +7,8 @@ sentences = ['This framework generates embeddings for each input sentence',
     'Sentences are passed as a list of string.', 
     'The quick brown fox jumps over the lazy dog.']
 sentence_embeddings = model.encode(sentences)
+
+for sentence, embedding in zip(sentences, sentence_embeddings):
+    print("Sentence:", sentence)
+    print("Embedding:", embedding)
+    print("")
