c10537467e5cd85a6876b8c78e28bce60625b3af
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-01-23 12:12:27 +0530

    Update P_O_S_Tagging.py

--- Feature_engineering/P_O_S_Tagging.py
+++ Feature_engineering/P_O_S_Tagging.py
@@ -1,1 +1,6 @@
+# Part of speech Tagging
+import nltk
+from nltk.tokenize import word_tokenize
 
+text = word_tokenize("Shakespeare was born and raised in Stratford-upon-Avon, Warwickshire. At the age of 18, he married Anne Hathaway, with whom he had three children: Susanna and twins Hamnet and Judith.")
+nltk.pos_tag(text)
