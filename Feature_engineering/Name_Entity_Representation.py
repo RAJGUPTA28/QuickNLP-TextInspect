b02570e341ea845a4c2d22ee2229afa087fbe856
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-01-23 12:12:44 +0530

    Update Name_Entity_Representation.py

--- Feature_engineering/Name_Entity_Representation.py
+++ Feature_engineering/Name_Entity_Representation.py
@@ -1,1 +1,6 @@
+import spacy
+from spacy import displacy
 
+doc = nlp('Shakespeare was born and raised in Stratford-upon-Avon, Warwickshire. At the age of 18, he married Anne Hathaway, with whom he had three children: Susanna and twins Hamnet and Judith.')
+
+displacy.render(doc, jupyter=True, style='ent')
