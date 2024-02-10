5c1bb44bb673f3b526bb33076b17c0719a48cf8a
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-02-10 11:18:47 +0530

    Update Flask_app.py

--- Deployment/Flask_app.py
+++ Deployment/Flask_app.py
@@ -5,3 +5,16 @@ import pickle
 
 
 
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/api',methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    prediction = model.predict([[np.array(data['exp'])]])
+    output = prediction[0]
+    return jsonify(output)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
