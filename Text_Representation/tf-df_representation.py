ceb3a8b701601a03fb958b98b0094b2b05c45dd0
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-02-20 12:47:07 +0530

    Update tf-df_representation.py

--- Text_Representation/tf-df_representation.py
+++ Text_Representation/tf-df_representation.py
@@ -10,6 +10,13 @@ string = [d0, d1]
 tfidf = TfidfVectorizer()
 result = tfidf.fit_transform(string)
 
+print('\nidf values:')
+for ele1, ele2 in zip(tfidf.get_feature_names(), tfidf.idf_):
+	print(ele1, ':', ele2)
+
+
+print('\nWord indexes:')
+print(tfidf.vocabulary_)
 
 
 print('\ntf-idf value:')
