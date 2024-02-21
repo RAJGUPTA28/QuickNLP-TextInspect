fff31d4271c8f3d3a96ece404e3c473c7f15ff32
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-02-21 17:55:49 +0530

    Update P_O_S_Tagging.py

--- Feature_engineering/P_O_S_Tagging.py
+++ Feature_engineering/P_O_S_Tagging.py
@@ -9,3 +9,16 @@ NOUN
 ADJ
 VERB
 ORG
+
+ADJ	adjective	
+ADP	adposition	
+ADV	adverb	really, already, still, early, now
+CONJ	conjunction	and, or, but, if, while, although
+DET	determiner, article	the, a, some, most, every, no, which
+NOUN	noun	year, home, costs, time, Africa
+NUM	numeral	twenty-four, fourth, 1991, 14:24
+PRT	particle	at, on, out, over per, that, up, with
+PRON	pronoun	he, their, her, its, my, I, us
+VERB	verb	is, say, told, given, playing, would
+.	punctuation marks	. , ; !
+X	other
